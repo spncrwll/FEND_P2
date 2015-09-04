9fa84cd6d0d47831cd4e85df610abf45bd22f821
spncrwll <spncrwll@gmail.com>
2015-09-03 20:43:58 -0500

    Submit ready

--- js/helper.js
+++ js/helper.js
@@ -50,7 +50,7 @@ var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="%link%" target="_blank">%data%< /a>';
+var HTMLonlineTitle = '<a href="%link%" target="_blank">%data%</a>';
 var HTMLonlineSchool = ' @<a href="%link%" target="_blank">%data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
